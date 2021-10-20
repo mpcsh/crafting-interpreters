77d3b656d84a8dc2e866e7c77a386dddb3484e1c
Mark Cohen <m@mpc.sh>
2021-10-20 01:42:14 -0700

    chapter 4 implementation complete!

--- rlox/src/main.rs
+++ rlox/src/main.rs
@@ -1,60 +1,78 @@
-use std::{env, fs, io, process};
+#![warn(clippy::all)]
+
+mod scanner;
+mod token;
+
+use std::{
+    env, fs,
+    io::{self, Write},
+    process,
+};
+
+use scanner::Scanner;
 
 struct Lox {
-    hadError: bool,
+    had_error: bool,
 }
 
 impl Lox {
-    fn runFile(&self, path: String) -> io::Result<()> {
+    pub fn new() -> Self {
+        Self { had_error: false }
+    }
+
+    pub fn run_file(&mut self, path: String) -> io::Result<()> {
         let chars = fs::read_to_string(path)?;
         self.run(chars);
-        if self.hadError {
+        if self.had_error {
             process::exit(65);
         }
         Ok(())
     }
 
-    fn runPrompt(&self) -> io::Result<()> {
-        let mut chars = String::new();
+    pub fn run_prompt(&mut self) -> io::Result<()> {
         loop {
             print!("> ");
+            io::stdout().flush()?;
+            let mut chars = String::new();
             match io::stdin().read_line(&mut chars) {
                 Err(e) => return Err(e),
                 Ok(0) => return Ok(()),
                 Ok(_) => {
                     self.run(chars);
-                    if self.hadError {
-                        process::exit(65);
-                    }
+                    self.had_error = false;
                 }
             }
         }
     }
 
-    fn run(&self, chars: String) {
-        let scanner = Scanner::new(chars);
-        let tokens = scanner.scanTokens();
+    fn run(&mut self, chars: String) {
+        let mut scanner = Scanner::new(chars);
+        let tokens = scanner.scan_tokens();
         println!("{:?}", tokens);
     }
 
-    fn error(&self, line: usize, message: String) {
+    fn error(&mut self, line: usize, message: String) {
         self.report(line, "".to_string(), message);
     }
 
-    fn report(&self, line: usize, location: String, message: String) {
+    fn report(&mut self, line: usize, location: String, message: String) {
         println!("[line {}] Error {}: {}", line, location, message);
-        self.hadError = true;
+        self.had_error = true;
     }
 }
 
-fn main() {
+fn main() -> io::Result<()> {
     let args = env::args().skip(1).collect::<Vec<_>>();
     if args.len() > 1 {
         println!("Usage: rlox [script]");
         process::exit(64);
-    } else if args.len() == 1 {
-        runFile(args[0]);
+    }
+
+    let mut lox = Lox::new();
+
+    if args.len() == 1 {
+        lox.run_file(args[0].to_string())
     } else {
-        runPrompt();
+        lox.run_prompt()
     }
 }
